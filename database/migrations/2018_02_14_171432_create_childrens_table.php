c9b0cc2e25ea8052319a33674414f4da26624d19
danistommi <N3v3mdan1stom1>
2018-02-14 18:41:42 +0100

    edited registration added childrens table

---
+++ database/migrations/2018_02_14_171432_create_childrens_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChildrensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+	    Schema::create('childrens', function (Blueprint $table) {
+	    	$table->increments('id');
+	    	$table->integer('parent_id')->unsigned();
+	    	$table->string('first_name');
+	    	$table->string('last_name');
+	    	$table->date('date_of_birth');
+	    	$table->enum('gender', ['male', 'female']);
+		    $table->foreign('parent_id')->references('id')->on('users');
+	    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+	    Schema::dropIfExists('childrens');
+    }
+}

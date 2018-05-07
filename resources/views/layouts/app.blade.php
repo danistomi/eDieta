354fd4efb38110ef7d92d79ed7391a43773ead3c
danistomi <danistomi@gmail.com>
2018-05-07 12:02:05 +0200

    fixed app blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
 
     <!-- Scripts -->
     <script>
-        window.Laravel =; <?php echo json_encode( [
+        window.Laravel = <?php echo json_encode( [
 		    'csrfToken' => csrf_token(),
 	    ] ); ?>
     </script>
